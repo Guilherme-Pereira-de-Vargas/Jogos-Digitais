ec02dc8ea870dac9b7bf0565ee3333b83ee356ab
Guilherme Pereira de Vargas <guilhermepvargas79@gmail.com>
2025-10-28 15:31:04 -0300

    Camera e Menu

---
+++ Jogos-Digitais/Assets/Scripts/CameraController.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public Transform Player;
+    public float time, minX, maxX;
+
+    private void FixedUpdate()
+    {
+        Vector3 newPosition = Player.position + new Vector3(0, 0, -10);
+        newPosition.y = 0.1f;
+        newPosition = Vector3.Lerp(transform.position, newPosition, time);
+        transform.position = newPosition;
+
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, minX, maxX), transform.position.y, transform.position.y);
+    }
+}
